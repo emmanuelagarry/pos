34db8fc0c03bf13d4884b1b6fbfe429e6767824a
Emmanuel Agarry <emmanuelagarry@gmail.com>
2021-02-23 22:23:08 +0100

    fake commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'xxx-MLd3MqgUDIZBk',
-    authDomain: 'xxxxx.com',
-    databaseURL: 'https://testsadyah.firebaseio.com',
-    projectId: 'xxxxx',
-    storageBucket: 'xxxx.appspot.com',
-    messagingSenderId: 'xxxx',
-    appId: '1:xxxxx:web:xxxxx',
-    measurementId: 'G-xxxx',
+    apiKey: 'AIzaSyDjRneFR5B99MPh8HxzuzLOhpBeZgjQ-w8',
+    authDomain: 'inventory-application-db.firebaseapp.com',
+    projectId: 'inventory-application-db',
+    storageBucket: 'inventory-application-db.appspot.com',
+    messagingSenderId: '155411699735',
+    appId: '1:155411699735:web:5868bdeb7cbbcb235da452',
+    measurementId: 'G-GF27PZH5G8',
   },
 }
 
